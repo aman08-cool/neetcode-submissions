1aee49c6e7101e9e20302d645925f88ec75ff422
Aman Dwivedi <84230636+aman08-cool@users.noreply.github.com>
2026-05-17 09:55:03 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    unordered_map<string, vector<string>> mp;
+    int index = 0;
+    string encode(vector<string>& strs) {
+        index++;
+        string id = to_string(index);
+        mp[id] = strs;
+        return id;
+    }
+
+    vector<string> decode(string s) {
+        return mp[s];
+    }
+};
