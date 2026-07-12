f3945b55f8c53dd672ead7eb78a15845d2b6df72
Aman Dwivedi <84230636+aman08-cool@users.noreply.github.com>
2026-07-12 15:25:57 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+   public:
+    ListNode* getKthNode(ListNode* curr, int k) {
+        while (curr && k > 1) {
+            curr = curr->next;
+            k--;
+        }
+        return curr;
+    }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* groupPrev = dummy;
+        while (true) {
+            ListNode* kth = getKthNode(groupPrev->next, k);
+            if (kth == NULL) break;
+            ListNode* groupNext = kth->next;
+            ListNode* prev = groupNext;
+            ListNode* curr = groupPrev->next;
+            while (curr != groupNext) {
+                ListNode* next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+            ListNode* temp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = temp;
+        }
+        return dummy->next;
+    }
+};
